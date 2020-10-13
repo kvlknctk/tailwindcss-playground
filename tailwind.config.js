b160d435419f6f9e0a3d1fcf63ac144a0bf74b5f
kvlknctk <iletisim@volkancatak.com.tr>
2020-10-13 03:20:57 +0300

    resumeç

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,25 @@
-const { colors } = require('tailwindcss/defaultTheme')
+const {colors} = require('tailwindcss/defaultTheme')
 
 
 module.exports = {
-  purge: ["./public/**/*.html"],
-  theme: {
-    extend: {
-      colors: {
-        apifiz: {
-          bg : '#f7f0ed'
-        }
-      }
+    purge   : ["./public/**/*.html"],
+    theme   : {
+        extend: {
+            colors  : {
+                apifiz: {
+                    bg     : '#f7f0ed',
+                    primary: '#6e65c0'
+                }
+            },
+            fontSize: {
+                '6xl': '4rem',
+                '7xl': '5rem'
+            },
+            height: {
+                'custom': '350px'
+            }
+        },
     },
-  },
-  variants: {},
-  plugins: [],
+    variants: {},
+    plugins : [],
 };
